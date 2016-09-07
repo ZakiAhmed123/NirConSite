c2d0a952d4d9b4330c7c9ce31fffb158e14443da
“Zaki <reach.zaki.ahmed@gmail.com>
2016-09-07 18:23:22 -0500

    configure email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['info.nircon'],
-   :password             => ENV['Sandeep01'],
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
    :authentication       => "plain",
    :enable_starttls_auto => true
     }
