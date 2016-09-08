0a08cda1092ade0c9206c013d4ecbcde62b2fd2b
“Zaki <reach.zaki.ahmed@gmail.com>
2016-09-08 14:20:05 -0500

    configuring rails mailer and fixed mandatory attachment bug

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,10 +3,13 @@ class MessageMailer < ApplicationMailer
   default :to => "info.nircon@gmail.com"
   def message_me(msg)
     @msg=msg
+    if @msg.file.present?
     attachments['file'] =  {
       :content=>@msg.file.read,
       :mime_type=>@msg.file.content_type
     }
+  else
+  end
     mail from: @msg.email, subject: @msg.subject, body: @msg.content
 
   end
